f40f3f0d69355cbbebab01bb140423e044713b96
9-max-6 <mutukumaxwel@gmail.com>
2024-07-24 19:36:22 +0300

    Chore: did the second question of the project by including the this keyword along with the declaration of the anonymous function.

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,7 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const addNeighborhood = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
