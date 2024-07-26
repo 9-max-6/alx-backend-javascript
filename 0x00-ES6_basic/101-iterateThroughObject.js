68b1e1758807f42d10234e47e6a387b65f97fb27
9-max-6 <mutukumaxwel@gmail.com>
2024-07-26 18:16:28 +0300

    Chore - final task of the project ES6-basics

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const allEmployees = [];
+
+  for (const emp of reportWithIterator) {
+    allEmployees.push(emp);
+  }
+
+  return allEmployees.join(" | ");
+}
