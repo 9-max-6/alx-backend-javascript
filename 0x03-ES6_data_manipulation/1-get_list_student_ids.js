d10cb20ca278aa6eac2736328afb3fd982326332
9-max-6 <mutukumaxwel@gmail.com>
2024-07-30 16:17:10 +0300

    Fix - eslint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,6 @@ export default function getListStudentIds(listOfStudentObjects) {
   if (typeof listOfStudentObjects === Array) {
     const listIds = listOfStudentObjects.map((student) => student.id);
     return listIds;
-  } else {
-    return [];
   }
+  return [];
 }
