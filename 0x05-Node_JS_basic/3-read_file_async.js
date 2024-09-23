470ac9b4d14297b6f6e2b796bfa3afa483cb6973
9-max-6 <mutukumaxwel@gmail.com>
2024-09-23 16:08:19 +0300

    Chore: testing the error in my code

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,47 +1,49 @@
-const fs = require('fs').promises;
+const fs = require("fs");
 
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    lines.shift();
+const countStudents = (dataPath) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(dataPath, "utf-8", (err, data) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+      }
+      if (data) {
+        const fileLines = data.toString("utf-8").trim().split("\n");
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(",");
+        const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-    if (lines.length === 0) {
-      throw new Error('No valid student data found in the file.');
-    }
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(",");
+          const studentPropValues = studentRecord.slice(
+            0,
+            studentRecord.length - 1
+          );
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+          const studentEntries = studentPropNames.map((propName, idx) => [
+            propName,
+            studentPropValues[idx],
+          ]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
+        }
 
-    const visited = [];
-    lines.forEach((line) => {
-      const fields = line.split(',');
-      if (fields.length < 4) return;
-      const subject = fields.at(-1).trim();
-      if (!visited.includes(subject)) {
-        visited.push(subject);
+        const totalStudents = Object.values(studentGroups).reduce(
+          (pre, cur) => (pre || []).length + cur.length
+        );
+        console.log(`Number of students: ${totalStudents}`);
+        for (const [field, group] of Object.entries(studentGroups)) {
+          const studentNames = group
+            .map((student) => student.firstname)
+            .join(", ");
+          console.log(
+            `Number of students in ${field}: ${group.length}. List: ${studentNames}`
+          );
+        }
+        resolve(true);
       }
     });
-
-    console.log(`Number of students: ${lines.length}`);
-
-    visited.forEach((subject) => {
-      const thisSubject = [];
-
-      lines.forEach((line) => {
-        const fields = line.split(',');
-        if (fields.length < 4) return;
-        if (fields.at(-1).trim() === subject) {
-          thisSubject.push(fields.at(0).trim());
-        }
-      });
-
-      console.log(
-        `Number of students in ${subject}: ${
-          thisSubject.length
-        }. List: ${thisSubject.join(', ')}`,
-      );
-    });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-}
+  });
 
 module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,6 @@
+const http = require("http");
+
+PORT = 1245;
+HOST = "127.0.0.1";
+
+app = http.createServer((req, res) => {});
